a83ce21057130a049d83c55b67a07ed17c683f90
Phil <philpem@philpem.me.uk>
2026-09-14 20:49:05 +0100

    Add geographic projection package API

---
+++ src/patchcreator/geography/__init__.py
@@ -0,0 +1,20 @@
+"""Geographic data and projection helpers for procedural celestial artwork."""
+
+from .natural_earth import load_natural_earth_land_rings, load_shapefile_rings
+from .projection import (
+    Viewpoint,
+    default_simplification_tolerance,
+    orthographic_point,
+    simplify_polyline,
+    visible_polyline_parts,
+)
+
+__all__ = [
+    "Viewpoint",
+    "default_simplification_tolerance",
+    "load_natural_earth_land_rings",
+    "load_shapefile_rings",
+    "orthographic_point",
+    "simplify_polyline",
+    "visible_polyline_parts",
+]
