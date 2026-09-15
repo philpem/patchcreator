d9dbf16770968a709cdb650d6fbbbcfee82f617b
Phil <philpem@philpem.me.uk>
2026-09-15 13:57:50 +0100

    Export narrow gap validator

--- src/patchcreator/validation/__init__.py
+++ src/patchcreator/validation/__init__.py
@@ -2,6 +2,7 @@
 
 from .engine import check_svg
 from .features import validate_small_features
+from .gaps import validate_narrow_gaps
 from .geometry import FilledGeometry, iter_visible_fills, polygon_components
 from .model import Finding, ValidationReport
 from .strokes import validate_minimum_stroke_width
@@ -16,5 +17,6 @@ __all__ = [
     "iter_visible_fills",
     "polygon_components",
     "validate_minimum_stroke_width",
+    "validate_narrow_gaps",
     "validate_small_features",
 ]
