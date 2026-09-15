3b2637ee0da80d5dc3b81fdb09d586649f54f6b1
Phil <philpem@philpem.me.uk>
2026-09-15 17:10:37 +0100

    Expose compatibility export API

--- src/patchcreator/svg/__init__.py
+++ src/patchcreator/svg/__init__.py
@@ -1,3 +1,18 @@
+from .export import (
+    CompatibilityExportError,
+    ExportOptions,
+    ExportResult,
+    export_svg_file,
+    export_svg_text,
+)
 from .writer import RenderResult, render_design
 
-__all__ = ["RenderResult", "render_design"]
+__all__ = [
+    "CompatibilityExportError",
+    "ExportOptions",
+    "ExportResult",
+    "RenderResult",
+    "export_svg_file",
+    "export_svg_text",
+    "render_design",
+]
