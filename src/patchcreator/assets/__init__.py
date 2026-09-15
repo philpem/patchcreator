1550a7a20e62a67403e8b76dcd66d9d417789c4e
Phil <philpem@philpem.me.uk>
2026-09-15 03:42:54 +0100

    Add asset normalizer public API

---
+++ src/patchcreator/assets/__init__.py
@@ -0,0 +1,5 @@
+"""Reusable SVG asset inspection and normalisation."""
+
+from .normalize import AssetReport, inspect_asset, normalize_asset
+
+__all__ = ["AssetReport", "inspect_asset", "normalize_asset"]
