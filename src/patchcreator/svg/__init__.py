61a9c549ab9bcc66dd178043bc18574aef410546
Phil <philpem@philpem.me.uk>
2026-09-16 22:37:27 +0100

    Expose text outline resolution metadata

--- src/patchcreator/svg/__init__.py
+++ src/patchcreator/svg/__init__.py
@@ -5,13 +5,17 @@ from .export import (
     export_svg_file,
     export_svg_text,
 )
+from .text_outline import FontResolution, TextOutlineError, TextOutlineResult
 from .writer import RenderResult, render_design
 
 __all__ = [
     "CompatibilityExportError",
     "ExportOptions",
     "ExportResult",
+    "FontResolution",
     "RenderResult",
+    "TextOutlineError",
+    "TextOutlineResult",
     "export_svg_file",
     "export_svg_text",
     "render_design",
