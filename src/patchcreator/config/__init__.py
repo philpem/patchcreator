e00d74e90497c56dfe3d2179bf7a891ba67b9554
Phil <philpem@philpem.me.uk>
2026-09-18 00:28:06 +0100

    Export design JSON Schema helpers

--- src/patchcreator/config/__init__.py
+++ src/patchcreator/config/__init__.py
@@ -1,4 +1,18 @@
+from .json_schema import (
+    DESIGN_SCHEMA_ID,
+    JSON_SCHEMA_DIALECT,
+    design_json_schema,
+    design_json_schema_text,
+)
 from .loader import DesignLoadError, load_design
 from .schema import DesignSpec
 
-__all__ = ["DesignLoadError", "DesignSpec", "load_design"]
+__all__ = [
+    "DESIGN_SCHEMA_ID",
+    "JSON_SCHEMA_DIALECT",
+    "DesignLoadError",
+    "DesignSpec",
+    "design_json_schema",
+    "design_json_schema_text",
+    "load_design",
+]
