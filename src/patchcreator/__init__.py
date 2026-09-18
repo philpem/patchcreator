19a3112d594874f3d504742b466fbca626878a2b
Phil <philpem@philpem.me.uk>
2026-09-18 12:38:42 +0100

    Resolve runtime version from package metadata

--- src/patchcreator/__init__.py
+++ src/patchcreator/__init__.py
@@ -2,6 +2,7 @@
 
 from .config.loader import DesignLoadError, load_design
 from .config.schema import DesignSpec
+from ._version import resolve_version
 
 __all__ = ["DesignLoadError", "DesignSpec", "load_design"]
-__version__ = "0.1.0.dev0"
+__version__ = resolve_version()
